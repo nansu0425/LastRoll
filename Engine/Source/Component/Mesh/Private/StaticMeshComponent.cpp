7f11d4f7f60bb2790c4e2236a80ad7734c0c811e
Donghee <luz0415@naver.com>
2025-10-13 02:58:31 +0900

    fix: StaticMesh 변경 후 Dirty 플래그 세워주기
    - 세워줘야 AABB 등 다시 계산

--- Engine/Source/Component/Mesh/Private/StaticMeshComponent.cpp
+++ Engine/Source/Component/Mesh/Private/StaticMeshComponent.cpp
@@ -114,6 +114,7 @@ void UStaticMeshComponent::SetStaticMesh(const FName& InObjPath)
 		RenderState.CullMode = ECullMode::Back;
 		RenderState.FillMode = EFillMode::Solid;
 		BoundingBox = &AssetManager.GetStaticMeshAABB(InObjPath);
+		MarkAsDirty();
 	}
 }
 
