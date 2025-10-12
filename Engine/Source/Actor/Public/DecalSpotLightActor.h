44bd10f79f5f24779aa1da7dd8671165a7fa8725
Donghee <luz0415@naver.com>
2025-10-13 01:58:21 +0900

    feat: DecalSpotLightActor Billboard Icon 추가

--- Engine/Source/Actor/Public/DecalSpotLightActor.h
+++ Engine/Source/Actor/Public/DecalSpotLightActor.h
@@ -6,11 +6,11 @@ class ADecalSpotLightActor : public AActor
 {
 	GENERATED_BODY()
 	DECLARE_CLASS(ADecalSpotLightActor, AActor)
-
-
+	
 public:
 	ADecalSpotLightActor();
 
 	virtual UClass* GetDefaultRootComponent() override;
+	virtual void InitializeComponents() override;
 };
 
