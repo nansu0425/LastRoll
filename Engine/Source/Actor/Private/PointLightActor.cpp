78e430b5c322504ef8260ecd7c56ef9bc4fc0a6f
Donghee <luz0415@naver.com>
2025-10-15 23:35:04 +0900

    add: HeightFogActor, PointLightActor 추가

---
+++ Engine/Source/Actor/Private/PointLightActor.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "Actor/Public/PointLightActor.h"
+#include "Component/Public/BillBoardComponent.h"
+#include "Component/Public/PointLightComponent.h"
+#include "Manager/Asset/Public/AssetManager.h"
+
+IMPLEMENT_CLASS(APointLightActor, AActor)
+
+APointLightActor::APointLightActor()
+{
+}
+
+UClass* APointLightActor::GetDefaultRootComponent()
+{
+    return UPointLightComponent::StaticClass();
+}
+
+void APointLightActor::InitializeComponents()
+{
+    Super::InitializeComponents();
+	
+    UBillBoardComponent* Billboard = CreateDefaultSubobject<UBillBoardComponent>();
+    Billboard->AttachToComponent(GetRootComponent());
+    Billboard->SetIsVisualizationComponent(true);
+    Billboard->SetSprite(UAssetManager::GetInstance().LoadTexture("Data/Icons/PointLight_64x.png"));
+    Billboard->SetScreenSizeScaled(true);
+}
---
+++ Engine/Source/Actor/Public/PointLightActor.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "Actor/Public/Actor.h"
+
+UCLASS()
+class APointLightActor : public AActor
+{
+    GENERATED_BODY()
+    DECLARE_CLASS(APointLightActor, AActor)
+	
+public:
+    APointLightActor();
+
+    virtual UClass* GetDefaultRootComponent() override;
+    virtual void InitializeComponents() override;
+};
