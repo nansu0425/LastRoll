1816645168908dc263f05b5cb821abbdd69cf13a
TeshShin <shintom1222@gmail.com>
2025-10-18 22:56:20 +0900

    fix: PointLight의 Falloff 0까지 줄어들 수 있도록

--- Engine/Source/Component/Public/PointLightComponent.h
+++ Engine/Source/Component/Public/PointLightComponent.h
@@ -52,7 +52,7 @@ public:
     float GetAttenuationRadius() const { return AttenuationRadius;}
     
     /** @note Sets the light falloff exponent and clamps it to the same range as Unreal Engine (2.0 - 16.0). */
-    void SetDistanceFalloffExponent(float InDistanceFalloffExponent) { DistanceFalloffExponent = std::clamp(InDistanceFalloffExponent, 2.0f, 16.0f); }
+    void SetDistanceFalloffExponent(float InDistanceFalloffExponent) { DistanceFalloffExponent = std::clamp(InDistanceFalloffExponent, 0.0f, 16.0f); } // 이지만, 최소 0이 되도록 해보자
     
     void SetAttenuationRadius(float InAttenuationRadius) { AttenuationRadius = InAttenuationRadius; }
 
