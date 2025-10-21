8b6d86c30184c82cc24303a757149a0f11fe5ff8
homing123 <k99960942@gmail.com>
2025-10-21 17:50:34 +0900

    fix: Cluster Gizmo Vertex Set 버그 수정

--- Engine/Asset/Shader/ClusterGizmoSetCS.hlsl
+++ Engine/Asset/Shader/ClusterGizmoSetCS.hlsl
@@ -78,19 +78,29 @@ void main( uint3 DTid : SV_DispatchThreadID )
     }
     
     FAABB CurAABB = ClusterAABB[ClusterIdx];
-    float4 WorldMin = mul(float4(CurAABB.Min, 1), ViewInv);
-    float4 WorldMax = mul(float4(CurAABB.Max, 1), ViewInv);
+    float3 ViewMin = CurAABB.Min;
+    float3 ViewMax = CurAABB.Max;
     
+    float3 ViewPos[8];
+    ViewPos[0] = float3(ViewMin.x, ViewMin.y, ViewMin.z);
+    ViewPos[1] = float3(ViewMin.x, ViewMax.y, ViewMin.z);
+    ViewPos[2] = float3(ViewMax.x, ViewMax.y, ViewMin.z);
+    ViewPos[3] = float3(ViewMax.x, ViewMin.y, ViewMin.z);
+    ViewPos[4] = float3(ViewMin.x, ViewMin.y, ViewMax.z);
+    ViewPos[5] = float3(ViewMin.x, ViewMax.y, ViewMax.z);
+    ViewPos[6] = float3(ViewMax.x, ViewMax.y, ViewMax.z);
+    ViewPos[7] = float3(ViewMax.x, ViewMin.y, ViewMax.z);
+
     Color.w = 1;
     uint VertexOffset = 8 * ClusterIdx + 8;
-    ClusterGizmoVertex[0 + VertexOffset].Pos = float3(WorldMin.x, WorldMin.y, WorldMin.z);
-    ClusterGizmoVertex[1 + VertexOffset].Pos = float3(WorldMin.x, WorldMax.y, WorldMin.z);
-    ClusterGizmoVertex[2 + VertexOffset].Pos = float3(WorldMax.x, WorldMax.y, WorldMin.z);
-    ClusterGizmoVertex[3 + VertexOffset].Pos = float3(WorldMax.x, WorldMin.y, WorldMin.z);
-    ClusterGizmoVertex[4 + VertexOffset].Pos = float3(WorldMin.x, WorldMin.y, WorldMax.z);
-    ClusterGizmoVertex[5 + VertexOffset].Pos = float3(WorldMin.x, WorldMax.y, WorldMax.z);
-    ClusterGizmoVertex[6 + VertexOffset].Pos = float3(WorldMax.x, WorldMax.y, WorldMax.z);
-    ClusterGizmoVertex[7 + VertexOffset].Pos = float3(WorldMax.x, WorldMin.y, WorldMax.z);
+    ClusterGizmoVertex[0 + VertexOffset].Pos = mul(float4(ViewPos[0], 1), ViewInv);
+    ClusterGizmoVertex[1 + VertexOffset].Pos = mul(float4(ViewPos[1], 1), ViewInv);
+    ClusterGizmoVertex[2 + VertexOffset].Pos = mul(float4(ViewPos[2], 1), ViewInv);
+    ClusterGizmoVertex[3 + VertexOffset].Pos = mul(float4(ViewPos[3], 1), ViewInv);
+    ClusterGizmoVertex[4 + VertexOffset].Pos = mul(float4(ViewPos[4], 1), ViewInv);
+    ClusterGizmoVertex[5 + VertexOffset].Pos = mul(float4(ViewPos[5], 1), ViewInv);
+    ClusterGizmoVertex[6 + VertexOffset].Pos = mul(float4(ViewPos[6], 1), ViewInv);
+    ClusterGizmoVertex[7 + VertexOffset].Pos = mul(float4(ViewPos[7], 1), ViewInv);
     ClusterGizmoVertex[0 + VertexOffset].Color = Color;
     ClusterGizmoVertex[1 + VertexOffset].Color = Color;
     ClusterGizmoVertex[2 + VertexOffset].Color = Color;
