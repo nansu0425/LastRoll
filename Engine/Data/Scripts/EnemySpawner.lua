ee6c99e2e58ec3eaa10db9b1a612c15f6652ae02
geb0598 <geb0598@gmail.com>
2025-11-03 03:15:32 +0900

    feat: 에네미 스포너 추가

---
+++ Engine/Data/Scripts/EnemySpawner.lua
@@ -0,0 +1,73 @@
+-- ==============================================================================
+-- EnemySpawner.lua
+-- ==============================================================================
+
+local TestPool = require("Data/Scripts/ActorPool")
+
+-- ==============================================================================
+-- 스포너 설정 
+-- ==============================================================================
+
+-- 스폰 주기
+local SpawnInterval = 2.0
+
+-- 스폰 반경
+local SpawnRadius = 30.0
+
+-- 스폰 최소 반경
+local MinSpawnRadius = 5.0
+
+local SpawnTimer = 0.0
+
+-- Called once when the Actor begins play
+function BeginPlay()
+    -- @todo 게임 매니저가 존재할 경우 Clear를 게임 매니저에서 처리한다.
+    TestPool:Clear()
+    
+    SpawnTimer = 0.0
+end
+
+-- Called every frame
+-- @param dt: Delta time in seconds
+function Tick(dt)
+    SpawnTimer = SpawnTimer + dt
+    
+    if SpawnTimer >= SpawnInterval then 
+        SpawnFromPool() 
+        
+        SpawnTimer = SpawnTimer - SpawnInterval
+    end
+end
+
+-- Called once when the Actor ends play
+function EndPlay()
+    print("Actor ending: " .. obj.UUID)
+end
+
+-- ==============================================================================
+-- 스폰 함수
+-- ==============================================================================
+
+function SpawnFromPool()
+    local PlayerPos = _G.PlayerData.PlayerPos
+    if not PlayerPos then 
+        print("EnemySpawner:SpawnFromPool - PlayerPos is not available.")
+        return
+    end
+
+    local SpawnedActor = TestPool:Get("AEnemy")
+    if not SpawnedActor then 
+        print("EnemySpawner:SpawnFromPool - Failed to get Enemy from Pool.")
+        return
+    end
+    
+    local Angle = Random(0, math.pi * 2)
+    
+    local Distance = Random(MinSpawnRadius, SpawnRadius)
+    
+    local SpawnX = PlayerPos.x + math.cos(Angle) * Distance
+    local SpawnY = PlayerPos.y + math.sin(Angle) * Distance
+    local SpawnZ = PlayerPos.z
+    
+    SpawnedActor.Location = Vector(SpawnX, SpawnY, SpawnZ)
+end
---
+++ Engine/Source/Game/Actor/Private/Enemy.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+
+#include "Game/Actor/Public/Enemy.h"
+
+#include "Component/Mesh/Public/StaticMeshComponent.h"
+#include "Component/Public/ScriptComponent.h"
+
+IMPLEMENT_CLASS(AEnemy, AActor)
+
+AEnemy::AEnemy()
+{
+    EnemyScriptComponent = CreateDefaultSubobject<UScriptComponent>();
+}
+
+UClass* AEnemy::GetDefaultRootComponent()
+{
+    return UStaticMeshComponent::StaticClass();
+}
+
+void AEnemy::InitializeComponents()
+{
+    AActor::InitializeComponents();
+    
+    auto StaticMeshComponent = Cast<UStaticMeshComponent>(GetRootComponent());
+    
+    StaticMeshComponent->SetStaticMesh("Data/Apple.obj");
+    
+    EnemyScriptComponent->SetScriptPath("Enemy.lua");
+}
